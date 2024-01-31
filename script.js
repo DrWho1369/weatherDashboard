6ef7827d2dffee349543a306882c63683f941cd5
Dr Who Baker <tbaker.bhb@gmail.com>
2024-01-31 11:18:23 +0000

    added on click even listener to search button

--- script.js
+++ script.js
@@ -27,5 +27,49 @@ $(document).ready(function () {
       });
   }
 
+  function displayCurrentWeather(data) {
+    $("#today").empty();
+    let icon = data.weather[0].icon;
+    let iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    let temp = data.main.temp;
+    let humidity = data.main.humidity;
+    let wind = data.wind.speed;
+    let tempC = (temp - 273.15).toFixed(2);
+
+    let todayWeather = $("<div id='todayWeather'>");
+
+    todayWeather.append("<h2 id='city'>" + city + "</h2>");
+    todayWeather.append(
+      "<div id='date'>" + currentDay.format("dddd, MMMM D, YYYY") + "</div>"
+    );
+    todayWeather.append("<div id='icon'><img src=" + iconURL + "></div>");
+    todayWeather.append("<div id='temp'>Temperature: " + tempC + "°C</div>");
+    todayWeather.append("<div id='humidity'>Humidity: " + humidity + "%</div>");
+    todayWeather.append("<div id='wind'>Wind Speed: " + wind + "MPH</div>");
+
+    $("#today").append(todayWeather);
+  }
+
+  $("#search-button").on("click", function (event) {
+    event.preventDefault();
+    city = $("#search-input").val();
+    if (city.trim() !== "") {
+      let history = JSON.parse(localStorage.getItem("cities")) || []; //
+      if (!history.includes(city)) {
+        history.unshift(city);
+        const maxHistory = 5;
+        if (history.length > maxHistory) {
+          history.pop();
+        }
+        localStorage.setItem("cities", JSON.stringify(history));
+      }
+
+      displayHistory();
+
+      getWeather();
+      getForecast();
+    }
+  });
+
   setDay();
 });
