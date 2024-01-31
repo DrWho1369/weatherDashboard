0dbca2092546cbce6a98a00810bc4e1b8c0c8c9f
Dr Who Baker <tbaker.bhb@gmail.com>
2024-01-31 12:19:01 +0000

    changed and refactored functions for efficiency

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+// Initial setup when the document is ready
 $(document).ready(function () {
-  // Declare variables
+  // Variables Decleration
   let dateEl = $("#date");
   let timeEl = $("#time");
   let todayEl = $("#today");
   let forecastEl = $("#forecast");
   let historyEl = $("#history");
-
   let APIKey = "cdff13d48c1ee8584b1173383a7f5341";
-
   let city;
   let currentURL;
-
+  let todayContainer = $("#today");
+  // Function to set the current date and time
   function setDay() {
     dateEl.text(dayjs().format("dddd, MMMM D, YYYY"));
     timeEl.text(dayjs().format("h:mm A"));
@@ -18,48 +18,65 @@ $(document).ready(function () {
 
   function weatherApiCall() {
     currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-    fetch(currentURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        displayCurrentWeather(data);
-      });
+    fetchAndDisplay(currentURL, displayCurrentWeather);
   }
 
   function forecastApiCall() {
     currentURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
+    fetchAndDisplay(currentURL, showForecast);
+  }
 
-    fetch(currentURL)
+  function fetchAndDisplay(url, callback) {
+    fetch(url)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        displayForecast(data);
+        callback(data);
       });
   }
 
   function displayCurrentWeather(data) {
-    $("#today").empty();
+    todayContainer.empty();
     let icon = data.weather[0].icon;
     let iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-    let temp = data.main.temp;
     let humidity = data.main.humidity;
     let wind = data.wind.speed;
-    let tempC = (temp - 273.15).toFixed(2);
+    let tempC = (data.main.temp - 273.15).toFixed(2);
 
-    let todayWeather = $("<div id='todayWeather'>");
+    let todayWeather = `
+    <div id='todayWeather'>
+      <h2 id='city'>${city}</h2>
+      <div id='date'>${dayjs().format("dddd, MMMM D, YYYY")}</div>
+      <div id='icon'><img src="${iconURL}"></div>
+      <div id='temp'>Temperature: ${tempC}°C</div>
+      <div id='humidity'>Humidity: ${humidity}%</div>
+      <div id='wind'>Wind Speed: ${wind}MPH</div>
+    </div>
+  `;
+    todayContainer.append(todayWeather);
+  }
 
-    todayWeather.append("<h2 id='city'>" + city + "</h2>");
-    todayWeather.append(
-      "<div id='date'>" + dayjs().format("dddd, MMMM D, YYYY") + "</div>"
-    );
-    todayWeather.append("<div id='icon'><img src=" + iconURL + "></div>");
-    todayWeather.append("<div id='temp'>Temperature: " + tempC + "°C</div>");
-    todayWeather.append("<div id='humidity'>Humidity: " + humidity + "%</div>");
-    todayWeather.append("<div id='wind'>Wind Speed: " + wind + "MPH</div>");
+  function showForecast(data) {
+    const forecast = $("#forecast").empty();
+    forecast.append("<h5 id='header5Day'>5 Day Forecast: </h5>");
 
-    $("#today").append(todayWeather);
+    for (let i = 7; i <= 39; i += 8) {
+      const { dt_txt, weather, main } = data.list[i];
+      const iconURL = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+      const tempC = (main.temp - 273.15).toFixed(2);
+
+      const forecastDay = $(`
+            <div class='forecastDay'>
+                <div class='date'>${dt_txt}</div>
+                <div class='icon'><img src=${iconURL}></div>
+                <div class='temp'>Temperature: ${tempC}°C</div>
+                <div class='humidity'>Humidity: ${main.humidity}%</div>
+            </div>
+        `);
+
+      forecast.append(forecastDay);
+    }
   }
 
   function displayHistory() {
@@ -99,5 +116,14 @@ $(document).ready(function () {
     }
   });
 
+  $("#history").on("click", ".historyButton", function (event) {
+    event.preventDefault();
+    city = $(event.target).text();
+    weatherApiCall();
+    forecastApiCall();
+  });
+
+  // Call code to set the initial day and display search history on page load
   setDay();
+  displayHistory();
 });
